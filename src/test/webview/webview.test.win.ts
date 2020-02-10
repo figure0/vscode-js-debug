3f7241bb7f897728770cf7f7fcd1b83b97500bbc
John Emau <John.Emau@microsoft.com>
2020-02-10 14:49:24 -0800

    add webview test

---
+++ src/test/webview/webview.test.win.ts
@@ -0,0 +1,42 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+import { itIntegrates } from '../testIntegrationUtils';
+import { ITestHandle } from '../test';
+
+// Note: WebView's are currently only supported on the Windows 10 platform
+// and require Edge (Chromium) to be installed.
+// Get Edge here: https://www.microsoftedgeinsider.com/
+describe('webview breakpoints', function() {
+  // webviews start slower than browsers
+  const timeout = 120 * 1000;
+  this.timeout(timeout);
+
+  async function waitForPause(p: ITestHandle, cb?: (threadId: string) => Promise<void>) {
+    const { threadId } = p.log(await p.dap.once('stopped'));
+    await p.logger.logStackTrace(threadId);
+    if (cb) await cb(threadId);
+    return p.dap.continue({ threadId });
+  }
+
+  itIntegrates('launched script', async ({ r }) => {
+    // Breakpoint in separate script set after launch
+    const p = await r.launchUrl('script.html', {
+      runtimeExecutable: r.workspacePath('webview/win/WebView2Sample.exe'),
+      // WebView2Sample.exe will launch about:blank
+      urlFilter: 'about:blank',
+      useWebView: true,
+      timeout,
+    });
+    p.load();
+    await waitForPause(p, async () => {
+      await p.dap.setBreakpoints({
+        source: { path: p.workspacePath('web/script.js') },
+        breakpoints: [{ line: 6 }],
+      });
+    });
+    await waitForPause(p);
+    p.assertLog();
+  });
+});
---
+++ testWorkspace/webview/win/README.md
@@ -0,0 +1,1 @@
+This WebView2Sample.exe source code can be found in `demo\webview`.
